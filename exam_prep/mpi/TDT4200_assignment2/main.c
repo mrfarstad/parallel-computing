12c7293a65f1a7a89e2e16551f8351ad92e78393
Martin Farstad <farstad2k@gmail.com>
2019-12-06 14:10:49 +0100

    WIP try to fix for non-dividable thread count

--- exam_prep/mpi/TDT4200_assignment2/main.c
+++ exam_prep/mpi/TDT4200_assignment2/main.c
@@ -10,16 +10,16 @@
 #define YSIZE_NEW YSIZE * 2
 #define IMAGE_SIZE_NEW XSIZE_NEW *YSIZE_NEW * 3
 
-void changeColor(uchar *image, int world_size) {
-  for (unsigned int i = 0; i < IMAGE_SIZE / world_size; i += 3) {
+void changeColor(uchar *image, int work_size) {
+  for (unsigned int i = 0; i < work_size; i += 3) {
     uchar tmp = image[i];
     image[i] = image[i + 2];
     image[i + 2] = tmp;
   }
 }
 
-void changeSize(uchar *image, uchar *new_image, int world_size) {
-  for (size_t i = 0; i < YSIZE / world_size; i++) {
+void changeSize(uchar *image, uchar *new_image, int world_size, int work_size) {
+  for (size_t i = 0; i < work_size; i++) {
     for (size_t j = 0; j < XSIZE * 3; j += 3) {
       size_t index = i * XSIZE * 3 + j;
       size_t new_index = 2 * (i * XSIZE_NEW * 3 + j);
@@ -45,39 +45,44 @@ int main() {
 
   uchar *image;
   uchar *new_image;
-  int *sendcounts;
-  int *recvcounts;
-  int *send_displs;
-  int *recv_displs;
+
+  int *sendcounts = malloc(world_size * sizeof(int));
+  int *recvcounts = malloc(world_size * sizeof(int));
+  int *send_displs = malloc(world_size * sizeof(int));
+  int *recv_displs = malloc(world_size * sizeof(int));
 
   if (world_rank == 0) {
     image = malloc(IMAGE_SIZE);
     new_image = malloc(IMAGE_SIZE * 4);
     readbmp("before.bmp", image);
-
-    sendcounts = malloc(world_size * sizeof(int));
-    recvcounts = malloc(world_size * sizeof(int));
-    send_displs = malloc(world_size * sizeof(int));
-    recv_displs = malloc(world_size * sizeof(int));
-
-    for (int i = 0; i < world_size; i++) {
-      sendcounts[i] = IMAGE_SIZE / world_size;
-      recvcounts[i] = IMAGE_SIZE_NEW / world_size;
-      send_displs[i] = i * (IMAGE_SIZE / world_size);
-      recv_displs[i] = i * (IMAGE_SIZE_NEW / world_size);
-    }
   }
 
-  uchar *tmp_image = malloc(IMAGE_SIZE / world_size);
-  uchar *tmp_image_new = malloc(IMAGE_SIZE_NEW / world_size);
+  for (int i = 0; i < world_size; i++) {
+    int size = IMAGE_SIZE / world_size;
+    int size_new = IMAGE_SIZE_NEW / world_size;
+    if (i < YSIZE % world_size) {
+      size += XSIZE * 3;
+      size_new += XSIZE_NEW * 3;
+    }
+
+    sendcounts[i] = size;
+    recvcounts[i] = size_new;
+    send_displs[i] = i * size;
+    recv_displs[i] = i * size_new;
+  }
+
+  uchar *tmp_image = malloc(sendcounts[world_rank]);
+  uchar *tmp_image_new = malloc(recvcounts[world_rank]);
 
   MPI_Scatterv(image, sendcounts, send_displs, MPI_UNSIGNED_CHAR, tmp_image,
-               IMAGE_SIZE / world_size, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+               sendcounts[world_rank], MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
 
-  changeColor(tmp_image, world_size);
-  changeSize(tmp_image, tmp_image_new, world_size);
+  changeColor(tmp_image, sendcounts[world_rank]);
 
-  MPI_Gatherv(tmp_image_new, IMAGE_SIZE_NEW / world_size, MPI_UNSIGNED_CHAR,
+  changeSize(tmp_image, tmp_image_new, world_size,
+             sendcounts[world_rank] / (3 * XSIZE));
+
+  MPI_Gatherv(tmp_image_new, recvcounts[world_rank], MPI_UNSIGNED_CHAR,
               new_image, recvcounts, recv_displs, MPI_UNSIGNED_CHAR, 0,
               MPI_COMM_WORLD);
 
